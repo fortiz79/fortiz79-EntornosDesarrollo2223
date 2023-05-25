0ac799b921c57c5321195b39f6f019af01931a7d
alumno <alumno@ldapserver.empresa.local>
2023-05-25 21:05:07 +0200

    segundo commit

--- JavaApplicationPruebaGitHub/src/javaapplicationpruebagithub/JavaApplicationPruebaGitHub.java
+++ JavaApplicationPruebaGitHub/src/javaapplicationpruebagithub/JavaApplicationPruebaGitHub.java
@@ -15,7 +15,7 @@ public class JavaApplicationPruebaGitHub {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hola");
+        System.out.println("hola cartagena");
     }
     
 }
